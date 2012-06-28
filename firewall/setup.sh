ad5303c75ee07a20919eb775bca04bbd3f3a0ce9
Ian Macalinao <ianmacalinao@gmail.com>
2012-06-28 14:30:49 -0500

    ...

--- firewall/setup.sh
+++ firewall/setup.sh
@@ -6,28 +6,25 @@ iptables -F
 iptables -X
 
 echo Setting default filter policy
-iptables -P INPUT DROP
-iptables -P OUTPUT DROP
-iptables -P FORWARD DROP
+iptables -t filter -P INPUT DROP 
+iptables -t filter -P FORWARD DROP 
+iptables -t filter -P OUTPUT DROP
 
 echo Allow unlimited traffic on loopback
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 
 echo allow http
-sudo iptables -A INPUT -p udp -m udp --sport 53 -j ACCEPT
-sudo iptables -A INPUT -p tcp -m tcp --sport 80 -j ACCEPT
+iptables -t filter -A OUTPUT -p tcp --dport 80 -j ACCEPT
+iptables -t filter -A INPUT -p tcp --dport 80 -j ACCEPT
 
 echo allow inbound
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 echo Allow incoming ssh only
-iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
+iptables -t filter -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -t filter -A OUTPUT -p tcp --dport 22 -j ACCEPT
 
 echo allow minecraft
 iptables -A INPUT -p tcp --dport 25565 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 25565 -j ACCEPT
-
-echo make sure nothing comes or goes out of this box
-iptables -A INPUT -j DROP
-iptables -A OUTPUT -j DROP
